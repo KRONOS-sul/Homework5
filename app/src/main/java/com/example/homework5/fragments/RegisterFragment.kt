f9fceb7169884b20d75b4ffc17b75e90b56b60ce
Sultan <93419011+KRONOS-sul@users.noreply.github.com>
2024-04-26 12:23:36 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework5/fragments/RegisterFragment.kt
@@ -0,0 +1,31 @@
+package com.example.homework5.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.homework5.R
+import com.example.homework5.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+    private lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.registerTv.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.loginFragment)
+        }
+    }
+
+}
